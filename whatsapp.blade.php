6e4b3e56d85e9aa78084addc53d05b9f8cc72cac
ivan-burgio <ivan.24.burgio@gmail.com>
2024-12-26 11:35:51 -0300

    first commit

---
+++ whatsapp.blade.php
@@ -0,0 +1,6 @@
+<div class="fixed bottom-8 left-8 z-20">
+    <a href="https://wa.me/{{ $phone }}?text=Hola,+te+contacto+desde+tu+sitio+web." target="_blank"
+        class="flex items-center justify-center w-16 h-16 bg-green-500 text-white rounded-full hover:bg-green-600 shadow-md shadow-green-950 focus:outline-none focus:ring focus:ring-green-400">
+        <i class="fab fa-whatsapp fa-2x"></i>
+    </a>
+</div>
